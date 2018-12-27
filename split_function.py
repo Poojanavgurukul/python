4d60dd4a2c08d8eee800017d400e72ac6ba4d107
Poojanavgurukul <pooja17@navgurukul.org>
2018-12-27 15:55:56 +0530

    some algo

--- split_function.py
+++ split_function.py
@@ -1,13 +1,15 @@
-user=raw_input("enter your text\n") 
-str_list=[]
-word=""
-for i in user:
-	if i==" ":
+def split(decorater):
+	user=raw_input("enter your text\n") 
+	str_list=[]
+	word=""
+	for i in user:
+		if i==decorater:
+			str_list.append(word)
+			word=""
+		else:
+			word +=i
+	if word !="":
 		str_list.append(word)
-		word=""
-	else:
-		word +=i
-if word !="":
-	str_list.append(word)
 
-print str_list
+	print str_list
+split("a")
